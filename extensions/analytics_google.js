078af0cd2d940be9157334d9435748b048cab771
michael <michaelc@zoovy.com>
2012-11-08 08:39:44 -0800

    Initial commit

--- extensions/analytics_google.js
+++ extensions/analytics_google.js
@@ -25,7 +25,10 @@ The functions here are designed to work with 'reasonable' size lists of categori
 var analytics_google = function() {
 	var r = {
 		
-		vars : {},
+		vars : {
+			"dependAttempts" : 0,  //used to count how many times loading the dependencies has been attempted.
+			"dependencies" : ['myRIA'] //a list of other extensions (just the namespace) that are required for this one to load
+			},
 
 ////////////////////////////////////   CALLBACKS    \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
@@ -33,17 +36,12 @@ var analytics_google = function() {
 		callbacks : {
 			init : {
 				onSuccess : function()	{
-					
 /*
 To keep this extension as self-contained as possible, it loads it's own script.
 the callback is handled in the extension loader. It will handle sequencing for the most part.
 The addTriggers will re-execute if this script isn't loaded until it has finished loading.
 */
 					app.u.loadScript(('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js');
-					if(zGlobals.checkoutSettings.googleCheckoutMerchantId)	{
-						app.u.loadScript(('https:' == document.location.protocol ? 'https://' : 'http://') + 'checkout.google.com/files/digital/ga_post.js'); //needed 4 tracking google wallet orders in GA.
-						}
-
 					return true;
 					},
 				onError : function()	{
@@ -55,23 +53,16 @@ The addTriggers will re-execute if this script isn't loaded until it has finishe
 
 			addTriggers : {
 				onSuccess : function(){
-//					app.u.dump("BEGIN analytics_google.callbacks.addTriggers.onSuccess");
-
 //make sure that not only has myRIA been loaded, but that the createTemplateFunctions has executed
 					if(app.ext.myRIA && app.ext.myRIA.template && typeof _gaq == 'object')	{
 
-//app.u.dump(" -> adding triggers");
+app.u.dump("BEGIN analytics_google.callbacks.addTriggers");
 app.ext.myRIA.template.homepageTemplate.onCompletes.push(function(P) {_gaq.push(['_trackPageview', '/index.html']);})
 app.ext.myRIA.template.categoryTemplate.onCompletes.push(function(P) {_gaq.push(['_trackPageview', '/category/'+P.navcat]);})
 app.ext.myRIA.template.productTemplate.onCompletes.push(function(P) {_gaq.push(['_trackPageview', '/product/'+P.pid]);})
 app.ext.myRIA.template.companyTemplate.onCompletes.push(function(P) {_gaq.push(['_trackPageview', '/company/'+P.show]);})
 app.ext.myRIA.template.customerTemplate.onCompletes.push(function(P) {_gaq.push(['_trackPageview', '/customer/'+P.show]);}) 
 app.ext.myRIA.template.checkoutTemplate.onInits.push(function(P) {_gaq.push(['_trackPageview', '/checkout']);}) 
-
-app.ext.myRIA.template.searchTemplate.onInits.push(function(P) {
-	pageTracker._trackPageview('/search?KEYWORDS='+P.KEYWORDS); 
-	}) 
-
 app.ext.myRIA.template.pageNotFoundTemplate.onCompletes.push(function(P) {_gaq.push(['_trackPageview', '/404.html?page=' + document.location.pathname + document.location.search + '&from=' + document.referrer]);})
 							
 app.ext.store_checkout.checkoutCompletes.push(function(P){
@@ -81,25 +72,26 @@ app.ext.store_checkout.checkoutCompletes.push(function(P){
 	_gaq.push(['_addTrans',
 		  P.orderID,           // order ID - required
 		  '', // affiliation or store name
-		  order['sum/order_total'],          // total - required
-		  order['sum/tax_total'],           // tax
-		  order['sum/ship_total'],          // shipping
-		  order['ship/city'],       // city
-		  order['ship/region'],     // state or province
-		  order['ship/countrycode']             // country
+		  order['data.order_total'],          // total - required
+		  order['data.tax_total'],           // tax
+		  order['ship.selected_price'],          // shipping
+		  order['data.ship_city'],       // city
+		  order['data.ship_state'],     // state or province
+		  order['data.ship_country']             // country
 	   ]);
 
-	var L = order['@ITEMS'].length;
-	app.u.dump(" -> "+L+" items in @ITEMS");
+	var L = order.stuff.length;
+	app.u.dump(" -> "+L+" items in stuff");
 
 	for(var i = 0; i < L; i += 1)	{
+//		app.u.dump(" -> "+i+": stid = "+order.stuff[i].stid+" and qty = "+order.stuff[i]['qty']);
 		_gaq.push(['_addItem',
 			P.orderID,         // order ID - necessary to associate item with transaction
-			order['@ITEMS'][i].product,         // SKU/code - required
-			order['@ITEMS'][i].prod_name,      // product name - necessary to associate revenue with product
-			order['@ITEMS'][i].stid, // category or variation
-			order['@ITEMS'][i].base_price,        // unit price - required
-			order['@ITEMS'][i].qty             // quantity - required
+			order.stuff[i].product,         // SKU/code - required
+			order.stuff[i].prod_name,      // product name - necessary to associate revenue with product
+			order.stuff[i].stid, // category or variation
+			order.stuff[i].base_price,        // unit price - required
+			order.stuff[i].qty             // quantity - required
 			]);
 		}
 	_gaq.push(['_trackTrans']);
